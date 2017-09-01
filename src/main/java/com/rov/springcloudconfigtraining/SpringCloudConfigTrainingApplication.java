c2a22f0939034006c60e6198cfa8f1f1c13413fc
rovsheneko <rovshennazarov@gmail.com>
2017-09-01 11:37:48 -0500

    added config code

---
+++ src/main/java/com/rov/springcloudconfigtraining/SpringCloudConfigTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.rov.springcloudconfigtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringCloudConfigTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigTrainingApplication.class, args);
+	}
+
+}
